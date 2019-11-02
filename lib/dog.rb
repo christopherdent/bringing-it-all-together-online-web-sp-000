5a1367a84aae802f355b85928cd57cc8381e8f8e
Christopher Dent <christopherdent01@gmail.com>
2019-11-02 19:26:07 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,50 @@
+require 'pry'
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id 
+  
+  def initialize(id:nil, name:, breed:)
+    @name = name 
+    @breed = breed 
+    @id = id 
+  end 
+  
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT, 
+      breed TEXT 
+      );
+    SQL
+    DB[:conn].execute(sql)
+  end 
+  
+  def self.drop_table
+    sql = <<-SQL 
+      DROP TABLE dogs
+    SQL
+    DB[:conn].execute(sql)
+  end 
+  
+  
+  def save 
+  
+    sql = <<-SQL 
+      INSERT INTO dogs (name, breed) 
+      VALUES (?, ?);
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+    #binding.pry
+    
+  end 
+  
+  def self.create
+    
+    
+    
+  end 
+
+    
+  
+end 
