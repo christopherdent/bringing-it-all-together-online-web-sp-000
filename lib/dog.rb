68aac23446aadd4c50de8b037a41cd5dd85031a1
Christopher Dent <christopherdent01@gmail.com>
2019-11-02 20:18:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,14 +29,14 @@ attr_reader :id
   
   
   def save 
-    if self.id 
-      self.update
+    #if self.id 
+    #  self.update
     else 
     sql = <<-SQL 
       INSERT INTO dogs (name, breed) 
       VALUES (?, ?);
     SQL
-    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute(sql, self.name, self.breed)
     #binding.pry
     self
   end 
