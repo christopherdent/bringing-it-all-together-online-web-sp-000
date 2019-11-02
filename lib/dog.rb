79af47226eda70a449293c89c74ce118433c5a5d
Christopher Dent <christopherdent01@gmail.com>
2019-11-02 19:55:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,21 +46,10 @@ attr_reader :id
   end
     
   def self.new_from_db(row)
-    
     new_dog = self.new(id: row[0], name: row[1], breed: row[2])   
-    #new_dog.id = row[0]
-    #new_dog.name =  row[1]
-    #new_dog.breed = row[2]
     new_dog 
   end 
-  
-  
-  
-  
-  
-  
-  
-  
+
   def self.find_by_id(id)
     sql = <<-SQL 
       SELECT * FROM dogs
