f27b977942040750e88b7baa1dc058f2772c7881
Christopher Dent <christopherdent01@gmail.com>
2019-11-02 20:01:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,7 +65,7 @@ attr_reader :id
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
         if !dog.empty?
       dog_data = dog[0]
-      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed:_data[2])
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
   end 
 
     
