76cfda75f0e6982f4e62eef806d4c03d0f52faed
Christopher Dent <christopherdent01@gmail.com>
2019-11-02 20:00:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,8 +64,8 @@ attr_reader :id
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
         if !dog.empty?
-      song_data = song[0]
-      song = Song.new(song_data[0], song_data[1], song_data[2])
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed:_data[2])
   end 
 
     
