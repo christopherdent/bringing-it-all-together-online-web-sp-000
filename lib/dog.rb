924f7bb6c5a31b9ba5786dd5df881bf53b734d35
Christopher Dent <christopherdent01@gmail.com>
2019-11-02 19:56:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,7 +57,7 @@ attr_reader :id
       LIMIT 1 
     SQL
     result = DB[:conn].execute(sql, id)[0]
-    Dog.new(result[0], result[1], result[2])
+    Dog.new(id: result[0], name: result[1], breed: result[2])
     
   end 
    
