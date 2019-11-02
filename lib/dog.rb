0f1a24eea545e340caa4516430869aea1a77a8b1
Christopher Dent <christopherdent01@gmail.com>
2019-11-02 20:02:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,11 +67,9 @@ attr_reader :id
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else 
-      Dog.new(name: name, breed: breed)
+      dog = Dog.new(name: name, breed: breed)
     end 
     dog 
-      
-    
   end 
 
     
