7d62bd25ce4e1dc46943b9495e402a26bd3f0345
Christopher Dent <christopherdent01@gmail.com>
2019-11-02 20:22:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,13 +29,15 @@ attr_reader :id
   
   
   def save 
+    #if self.id 
+    #  self.update
+    #else 
     sql = <<-SQL 
-    INSERT INTO students (name, grade) 
-    VALUES (?, ?)
+      INSERT INTO dogs (name, breed) 
+      VALUES (?, ?);
     SQL
-    DB[:conn].execute(sql, self.name, self.grade) 
-
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    @id = DB[:conn].execute(sql, self.name, self.breed)
+    binding.pry
     self
   end 
   
