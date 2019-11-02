9de7adc2972e982105ac87f1c82280436c912e83
Christopher Dent <christopherdent01@gmail.com>
2019-11-02 19:39:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,11 +40,24 @@ attr_reader :id
   end 
   
   def self.create(name:, breed:)
-    dog = Dog.new(name, breed)
+    dog = Dog.new(name:, breed:)
     dog.save
     dog
   end
     
+    
+      describe ".create" do
+    it 'takes in a hash of attributes and uses metaprogramming to create a new dog object. Then it uses the #save method to save that dog to the database'do
+      Dog.create(name: "Ralph", breed: "lab")
+      expect(DB[:conn].execute("SELECT * FROM dogs")).to eq([[1, "Ralph", "lab"]])
+    end
+    it 'returns a new dog object' do
+      dog = Dog.create(name: "Dave", breed: "podle")
+
+      expect(teddy).to be_an_instance_of(Dog)
+      expect(dog.name).to eq("Dave")
+    end
+  end
     
    
 
