f35aedf67ac4f1ff27d456f9ad1d81c4f2c7bbcd
Christopher Dent <christopherdent01@gmail.com>
2019-11-02 19:33:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 require 'pry'
 class Dog
-  attr_accessor :name, :breed
-  attr_reader :id 
-  
-  def initialize(id=nil, name:, breed:)
-    @name = name 
-    @breed = breed 
-    @id = id 
-  end 
+attr_accessor :name, :breed
+attr_reader :id
+ 
+  def initialize(id=nil, name, breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end
   
   def self.create_table
     sql = <<-SQL 
