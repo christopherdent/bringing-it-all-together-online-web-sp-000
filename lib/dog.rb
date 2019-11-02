e6d1539f1e39f71e905d63b1e81e09ec64af65cb
Christopher Dent <christopherdent01@gmail.com>
2019-11-02 19:32:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,7 +34,7 @@ class Dog
       INSERT INTO dogs (name, breed) 
       VALUES (?, ?);
     SQL
-    DB[:conn].execute(sql, self.name, self.breed)
+    DB[:conn].execute(sql)
     #binding.pry
     
   end 
